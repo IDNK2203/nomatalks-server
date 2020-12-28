7f964bb2aa5286d1483a63ae592c9f47ec9c2b71
idnk <efeidukpaye@gmail.com>
2020-12-28 13:17:09 +0100

    renamed dir named from Admin to admin

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "production") {
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
+// var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const methodOverride = require("method-override");
 const ejsLayouts = require("express-ejs-layouts");
@@ -46,7 +46,6 @@ app.use(methodOverride("_method"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // primary middlewares
